43bea701d453ce355da05379130d71f1674f0f84
Sebastian906 <elsebas1912@gmail.com>
2026-05-11 12:28:38 -0500

    Implementación de APIs POST y GET para los datos de partidos

--- sportz-express/src/index.js
+++ sportz-express/src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { matchRouter } from './routes/matches.js';
 
 const app = express();
 const port = 8000;
@@ -8,6 +9,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Welcome to Sportz Express API!');
 });
+
+app.use('/matches', matchRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
